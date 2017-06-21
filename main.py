2b8c157ee4ed0319952013b3976907337352fb6c
Eric <eric.michaud99@gmail.com>
2017-06-21 21:38:15 +0900

    formatted properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from __future__ import division
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
 
 class Model (object):
 	def __init__ (self):
@@ -24,6 +24,37 @@ class Model (object):
 			self.b = self.b - alpha * nabla_b
 			print "Loss = {}".format(self.cost(xs, ys))
 
-if __name__ == "main":
+def get_points(PATH):
+	data = np.genfromtxt(PATH, delimiter=',') #loads the csv into an array
+	print data.shape
+	subjects = []
+	n = 1
+	while n < len(data):
+		subject_num = data[n][1]
+		intel_avg = 0
+		match_avg = 0
+		potential_partners = 0
+		while data[n][1] == subject_num and n < len(data):
+			intel_avg += data[n][2]
+			match_avg += data[n][3]
+			potential_partners += 1
+			n += 1
+		intel_avg /= potential_partners
+		match_avg /= potential_partners
+		subjects.append((intel_avg, match_avg))
+	intels = np.array([d[0] for d in subjects])
+	intels = (intels - np.mean(intels)) / np.std(intels)
+	matches = np.array([d[1] for d in subjects])
+	matches = (matches - np.mean(matches)) / np.std(matches)
+	subjects_normalized = [(x, y) for x, y in zip(intels, matches)]
+	return subjects_normalized
+
+if __name__ == "__main__":
+	points = get_points('data.csv')
+	print "points loaded" #improve formatting later, ERIC
 	m = Model()
 	print "model created!"
+	xs = [d[0] for d in points]
+	ys = [d[1] for d in points]
+	plt.scatter(xs, ys)
+	plt.show()
